3c2867a65eafb6d256679b5400e9bc46e27f40cb
Yujie Wang <yujiewang@berkeley.edu>
2022-02-06 12:26:19 -0800

    removed janky div cloning logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,41 @@
-function convertPlaceholdersInSection(placeholderSelector, section){
-    $(placeholderSelector, section).each((index, item) => {
-        let newCard = $(".card-selector").clone(true);
-        let itemTitle = $(".placeholder-card-title", item);
-        let itemContents = $(".placeholder-card-contents", item);
-        let itemImageSrc = $(item).attr("data-image-src");
+function convertPlaceholdersInSection(placeholderSelector, sectionSelector){
+    $(placeholderSelector, sectionSelector).each((_, item) => {
 
-        $(".card-title", newCard).replaceWith(itemTitle);
-        $(".card-contents", newCard).replaceWith(itemContents);
-        newCard.attr("data-image-src", itemImageSrc);
-        newCard.removeClass("card-selector");
+        const itemTitle = $(".placeholder-card-title", item);
+        const itemContents = $(".placeholder-card-contents", item);
+        const itemImageSrc = $(item).attr("data-image-src");
 
-        newCard.appendTo(section);
+
+        let newCard = `
+            <div class="card-height parallax-window mt-2 mb-4 py-5 border border-2 border-deepblue d-flex justify-content-center align-items-end"
+            data-parallax="scroll" data-image-src= ${ itemImageSrc } data-position-y="-200px" data-speed="0.8"
+            ios-fix="true" android-fix="true">
+                <div class="col-auto card showcase-card p-5 mb-2 py-2 m-5 w-90 h-50 text-white bg-deepblue-faded overflow-auto">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            ${ itemTitle.html() }
+                        </h5>
+                        <p class="card-contents">
+                            ${ itemContents.html() }
+                        </p>
+                    </div>
+                </div>
+            </div>
+        `;
+        
+        $(sectionSelector).append(newCard);
+        item.remove();
     });
+}
+
+function applySimpleParallax(selector){
+    var images = document.querySelectorAll(selector);
+    console.log(images);
+    new simpleParallax(images);
 }
 
 $(function(){
     convertPlaceholdersInSection(".placeholder-card-selector", "#extracurriculars-showcase-section");
     convertPlaceholdersInSection(".placeholder-card-selector", "#experience-showcase-section");
+    //applySimpleParallax(".card-selector");
 });
